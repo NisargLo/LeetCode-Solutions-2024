3aebe210df8cb3cc5bcc43497ae5001da6c28e8a
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-02-22 17:40:53 +0530

    22-2-24

---
+++ Easy/Find the Town Judge.java
@@ -0,0 +1,18 @@
+class Solution {
+     public int findJudge(int n, int[][] trust) {
+         boolean f=false;
+         int a;
+         for(int i=0;i<trust.length;i++){
+                 a=trust[i][1];
+             for(int j=0;j<2;j++){
+                 if(a==trust[i][0]){
+                     break;
+                 }
+                 else{
+                     f=true;
+                 }
+             }
+         }
+         return a;
+     }
+}
