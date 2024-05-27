e029f8f84f0f6c6954238b9ce69334e93e711bf1
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-05-27 14:02:26 +0530

    Update Valid Palindrome II.java

--- Easy/Valid Palindrome II.java
+++ Easy/Valid Palindrome II.java
@@ -1,29 +1,26 @@
-public class Solution {
+class Solution {
      public boolean validPalindrome(String s) {
-          if (s.equals(" ")) {
-               return true;
+          int l = 0, r = s.length() - 1;
+          while (l < r) {
+               char lc = s.charAt(l);
+               char rc = s.charAt(r);
+               if (lc != rc)
+                    return compare(s, l + 1, r) || compare(s, l, r - 1);
+               l++;
+               r--;
           }
-          boolean b = true;
-          String s1 = new String(s);
-          int start, end, start1, end1;
-          byte i;
-          for (start = 0, end = s.length() - 1; start <= (s.length() / 2); start++, end--) {
-               if (s.charAt(start) != s.charAt(end)) {
-                    for (i = 0; i < s.length(); i++) {
-                         String c = new String(s.charAt(i) + "");
-                         s = s1.replaceAll(c, "");
-                         for (start1 = 0, end1 = s.length() - 1; start1 <= (s.length() / 2); start1++, end1--) {
-                              if (s.charAt(start1) != s.charAt(end1)) {
-                                   b = false;
-                              }
-                         }
-                         if (b == false && i == s1.length()) {
-                              return false;
-                         } else {
-                              return true;
-                         }
-                    }
+          return true;
+     }
+
+     public boolean compare(String s, int l, int r) {
+          while (l < r) {
+               char lc = s.charAt(l);
+               char rc = s.charAt(r);
+               if (lc != rc) {
+                    return false;
                }
+               l++;
+               r--;
           }
           return true;
      }
