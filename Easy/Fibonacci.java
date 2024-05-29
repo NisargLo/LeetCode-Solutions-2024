2b8d38aad3b7160eb661845a4442fd1db319c8f2
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-05-29 13:24:18 +0530

    29-5-24

---
+++ Easy/Fibonacci.java
@@ -0,0 +1,20 @@
+class Solution {
+     public int a = 0, b = 1, c;
+
+     public int fib(int n) {
+          if (n < 2) {
+               return n;
+          }
+          return fibo(n);
+     }
+
+     public int fibo(int n) {
+          if (n > 1) {
+               c = a + b;
+               a = b;
+               b = c;
+               fibo(n - 1);
+          }
+          return c;
+     }
+}
