fdb68faf3158301af911a2b902eda121d4411e2c
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-07-11 20:57:13 +0530

    Update Fibonacci.java

--- Easy/Fibonacci.java
+++ Easy/Fibonacci.java
@@ -1,20 +1,28 @@
 class Solution {
-     public int a = 0, b = 1, c;
-
      public int fib(int n) {
-          if (n < 2) {
-               return n;
-          }
-          return fibo(n);
+          double phi = (1 + Math.sqrt(5)) / 2;
+          return (int)(Math.round((Math.pow(phi, n) - Math.pow(1 - phi, n)) / Math.sqrt(5)));
      }
 
-     private int fibo(int n) {
-          if (n >= 2) {
-               c = a + b;
-               a = b;
-               b = c;
-               fibo(n - 1);
-          }
-          return c;
-     }
+     
+     // OR
+
+     // public int a = 0, b = 1, c;
+
+     // public int fib(int n) {
+     //      if (n < 2) {
+     //           return n;
+     //      }
+     //      return fibo(n);
+     // }
+
+     // private int fibo(int n) {
+     //      if (n >= 2) {
+     //           c = a + b;
+     //           a = b;
+     //           b = c;
+     //           fibo(n - 1);
+     //      }
+     //      return c;
+     // }
 }
